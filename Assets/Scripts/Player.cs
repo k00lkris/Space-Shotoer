8222c13d29a0adaa2c9d0fd92fcb13a0c9c29f96
k00lkris <73762280+k00lkris@users.noreply.github.com>
2021-08-02 13:00:46 -0500

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference
+    // data type (int, float, bool, string)
+    // every variable has a name
+    // optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private float _speedMultiplier = 2;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
+    private GameObject _shieldActivePrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+    [SerializeField]
+    private bool _isTripleShotActive = false;
+    [SerializeField]
+    private bool _isSpeedBoostAcive = false;
+    [SerializeField]
+    private bool _isShieldActive = false;
+
+    private SpawnManager _spawnManager;
+
+    [SerializeField]
+    private GameObject _shieldVisualizer;
+
+    [SerializeField]
+    private int _score;
+
+    private UIManager _uiManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>(); //find the obejct. get the component
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        
+        if(_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL");
+        }
+
+        if(_uiManager == null)
+        {
+            Debug.LogError("The UI Manager is NULL.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(!_isSpeedBoostAcive)
+            transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+        else
+        {
+            transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * (_speed * _speedMultiplier) * Time.deltaTime);
+        }
+
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+
+        if (_isTripleShotActive == true)
+        {
+            Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+
+
+    }
+
+    public void Damage()
+    {
+        //if shields is active
+        //do nothing...
+        //deactivate shields
+        //return;
+
+        if (!_isShieldActive)
+        {
+            _lives--;
+
+            _uiManager.UpdateLives(_lives);
+
+            if (_lives < 1)
+            {
+                _spawnManager.OnPlayerDeath();
+                Destroy(this.gameObject);
+            }
+            // disable the visualizer
+        }
+        else
+        {
+            _isShieldActive = false;
+            _shieldVisualizer.SetActive(false);
+            return;
+        }
+    }
+
+    public void TripleShotActive()
+    {
+        _isTripleShotActive = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+    IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isTripleShotActive = false;
+    }
+
+    public void SpeedBoostActive()
+    {
+        _isSpeedBoostAcive = true;
+        _speed *= _speedMultiplier;
+        StartCoroutine(SpeedBoostPowerDownRoutine());
+    }
+    
+    IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isSpeedBoostAcive = false;
+        _speed /= _speedMultiplier;
+    }
+
+    public void ShieldActive()
+    {
+        _isShieldActive = true;
+        //StartCoroutine(ShieldPowerDownRoutine());
+        _shieldVisualizer.SetActive(true);
+        
+        //enable the shields visualizer
+        
+    }
+
+    // Code to set shields for a limited time
+    //IEnumerator ShieldPowerDownRoutine()
+    //{
+    //    yield return new WaitForSeconds(5.0f);
+    //    _isShieldActive = false;
+    //}
+    public void AddScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdateScore(_score);
+    }
+}
